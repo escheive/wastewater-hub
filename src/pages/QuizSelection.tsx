fe2087ffaa511c3249c7f326a2dcc8be5c6c3da5
escheive <scheiveerik@gmail.com>
2025-05-19 01:32:35 -0700

    removed unused dependencies

--- src/pages/QuizSelection.tsx
+++ src/pages/QuizSelection.tsx
@@ -1,5 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom'
-import { Button } from "@/components/ui/button"
+import { useNavigate } from 'react-router-dom'
 import { MyButton } from '@/components/ui/MyButton'
 
 
