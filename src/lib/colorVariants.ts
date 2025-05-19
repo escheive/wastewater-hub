a42658be8b9a372fda0fae9d68ba45dac0c25f1f
escheive <scheiveerik@gmail.com>
2025-05-19 01:38:22 -0700

    added colorVariants lib file

---
+++ src/lib/colorVariants.ts
@@ -0,0 +1,90 @@
+export const colorVariants = {
+  red: {
+    bg: "bg-red-100",
+    text: "text-red-700"
+  },
+  orange: {
+    bg: "bg-orange-100",
+    text: "text-orange-700"
+  },
+  amber: {
+    bg: "bg-amber-100",
+    text: "text-amber-700"
+  },
+  yellow: {
+    bg: "bg-yellow-100",
+    text: "text-yellow-700"
+  },
+  lime: {
+    bg: "bg-lime-100",
+    text: "text-lime-700"
+  },
+  green: {
+    bg: "bg-green-100",
+    text: "text-green-700"
+  },
+  emerald: {
+    bg: "bg-emerald-100",
+    text: "text-emerald-700"
+  },
+  teal: {
+    bg: "bg-teal-100",
+    text: "text-teal-700"
+  },
+  cyan: {
+    bg: "bg-cyan-100",
+    text: "text-cyan-700"
+  },
+  sky: {
+    bg: "bg-sky-100",
+    text: "text-sky-700"
+  },
+  blue: {
+    bg: "bg-blue-100",
+    text: "text-blue-700"
+  },
+  indigo: {
+    bg: "bg-indigo-100",
+    text: "text-indigo-700"
+  },
+  violet: {
+    bg: "bg-violet-100",
+    text: "text-violet-700"
+  },
+  purple: {
+    bg: "bg-purple-100",
+    text: "text-purple-700"
+  },
+  fuchsia: {
+    bg: "bg-fuchsia-100",
+    text: "text-fuchsia-700"
+  },
+  pink: {
+    bg: "bg-pink-100",
+    text: "text-pink-700"
+  },
+  rose: {
+    bg: "bg-rose-100",
+    text: "text-rose-700"
+  },
+  slate: {
+    bg: "bg-slate-100",
+    text: "text-slate-700"
+  },
+  gray: {
+    bg: "bg-gray-100",
+    text: "text-gray-700"
+  },
+  zinc: {
+    bg: "bg-zinc-100",
+    text: "text-zinc-700"
+  },
+  neutral: {
+    bg: "bg-neutral-100",
+    text: "text-neutral-700"
+  },
+  stone: {
+    bg: "bg-stone-100",
+    text: "text-stone-700"
+  }
+} as const
